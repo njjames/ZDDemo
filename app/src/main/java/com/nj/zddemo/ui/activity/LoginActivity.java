9ef768417f4f4427317c52ce44f4cf555eea417e
niejun <niejun@qq.com>
2018-07-25 19:20:16 +0800

    增加清除操作员姓名功能

--- app/src/main/java/com/nj/zddemo/ui/activity/LoginActivity.java
+++ app/src/main/java/com/nj/zddemo/ui/activity/LoginActivity.java
@@ -2,33 +2,71 @@ package com.nj.zddemo.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.haoge.easyandroid.easy.EasyToast;
 import com.nj.zddemo.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText mLoginPass;
     private ImageView mShowPass;
+    private EditText mLoginName;
+    private ImageView mClearName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        initView();
+    }
+
+    private void initView() {
+        mLoginName = findViewById(R.id.et_login_name);
+        mClearName = findViewById(R.id.iv_clearname);
         mLoginPass = findViewById(R.id.et_login_pass);
         mShowPass = findViewById(R.id.iv_showpass);
+
+        mLoginName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!TextUtils.isEmpty(mLoginName.getText().toString())) {
+                    mClearName.setVisibility(View.VISIBLE);
+                } else {
+                    mClearName.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mClearName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLoginName.getText().clear();
+            }
+        });
         mShowPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mLoginPass.getInputType() == InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+                if (mLoginPass.getInputType() == InputType.TYPE_TEXT_VARIATION_PASSWORD + InputType.TYPE_CLASS_TEXT) {
                     mLoginPass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    mShowPass.setImageResource(R.drawable.eye_on);
                 } else {
-                    mLoginPass.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    mLoginPass.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+                    mShowPass.setImageResource(R.drawable.eye_off);
                 }
             }
         });
