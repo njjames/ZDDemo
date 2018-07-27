96e0599e051116933e47905bd76025d227f83642
niejun <niejun@qq.com>
2018-07-27 18:28:49 +0800

    完成设置IP对话框的界面和主题

--- app/src/main/java/com/nj/zddemo/ui/activity/LoginActivity.java
+++ app/src/main/java/com/nj/zddemo/ui/activity/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.nj.zddemo.ui.activity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nj.zddemo.R;
@@ -28,6 +32,17 @@ public class LoginActivity extends BaseMVPActivity {
     private TextView mOnLine;
     private TextView mIemi;
     private TextView mHelp;
+    private CheckBox mCbSetip1;
+    private CheckBox mCbSetip2;
+    private EditText mLoginServer;
+    private EditText mLoginServerNo;
+    private EditText mLoginServerPass;
+    private EditText mLoginServerPort;
+    private EditText mLoginServerSuffix;
+    private LinearLayout mLlServerNo;
+    private LinearLayout mLlServerPass;
+    private LinearLayout mLlServerPort;
+    private LinearLayout mLlServerSuffix;
 
     @Override
     protected int getLayoutId() {
@@ -85,20 +100,15 @@ public class LoginActivity extends BaseMVPActivity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.iv_clearname: //点击清除操作员名称
-                mLoginName.getText().clear();
+                clearName();
                 break;
             case R.id.iv_showpass: //点击是否显示密码
-                if (mLoginPass.getInputType() == InputType.TYPE_TEXT_VARIATION_PASSWORD + InputType.TYPE_CLASS_TEXT) {
-                    mLoginPass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    mShowPass.setImageResource(R.drawable.eye_on);
-                } else {
-                    mLoginPass.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
-                    mShowPass.setImageResource(R.drawable.eye_off);
-                }
+                changePassStatus();
                 break;
             case R.id.btn_login:
                 break;
             case R.id.tv_login_setip:
+                showSetIpDialog();
                 break;
             case R.id.tv_login_online:
                 break;
@@ -106,6 +116,49 @@ public class LoginActivity extends BaseMVPActivity {
                 break;
             case R.id.tv_login_help:
                 break;
+        }
+    }
+
+    /**
+     * 显示设置IP的对话框
+     */
+    private void showSetIpDialog() {
+        View view = LayoutInflater.from(this).inflate(R.layout.setip_dialog_layout, null);
+        Dialog dialog = new Dialog(this, R.style.MyDialog);
+        dialog.setContentView(view);
+        dialog.show();
+
+        mCbSetip1 = view.findViewById(R.id.cb_setip1);
+        mCbSetip2 = view.findViewById(R.id.cb_setip2);
+        mLoginServer = view.findViewById(R.id.et_login_server);
+        mLoginServerNo = view.findViewById(R.id.et_login_server_no);
+        mLoginServerPass = view.findViewById(R.id.et_login_server_pass);
+        mLoginServerPort = view.findViewById(R.id.et_login_server_port);
+        mLoginServerSuffix = view.findViewById(R.id.et_login_server_suffix);
+        mLlServerNo = view.findViewById(R.id.ll_server_no);
+        mLlServerPass = view.findViewById(R.id.ll_server_pass);
+        mLlServerPort = view.findViewById(R.id.ll_server_port);
+        mLlServerSuffix = view.findViewById(R.id.ll_server_suffix);
+
+    }
+
+    /**
+     * 清除操作员名称
+     */
+    private void clearName() {
+        mLoginName.getText().clear();
+    }
+
+    /**
+     * 修改密码的显示状态，是否加密显示
+     */
+    private void changePassStatus() {
+        if (mLoginPass.getInputType() == InputType.TYPE_TEXT_VARIATION_PASSWORD + InputType.TYPE_CLASS_TEXT) {
+            mLoginPass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            mShowPass.setImageResource(R.drawable.eye_on);
+        } else {
+            mLoginPass.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+            mShowPass.setImageResource(R.drawable.eye_off);
         }
     }
 }
